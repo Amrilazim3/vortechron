b9c04abcca5c3ca22cf25cee8866f1f50c0d9640
Amrilazim3 <a.azim0711@gmail.com>
2022-02-08 13:04:56 +0800

    adding method for remove file

--- app/Http/Controllers/User/ProfileController.php
+++ app/Http/Controllers/User/ProfileController.php
@@ -4,15 +4,18 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Exception;
 use Illuminate\Validation\Rule;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 
 class ProfileController extends Controller
 {
     public function edit(Request $request)
     {
-        $validated = $request->validate([
+        $request->validate([
             'name' => 'required|min:6',
             'username' => 'required|min:6',
             'bio' => 'max:50',
@@ -24,21 +27,43 @@ class ProfileController extends Controller
             'file' => 'nullable|image|file'
         ]);
 
-        //update database
         $user = Auth::user();
         
+        //update database
         $user->update([
             'name' => $request->name,
             'username' => $request->username,
             'bio' => $request->bio,
             'email' => $request->email,
             'image_url' => $request->hasFile('file') ? 
-                $request->file('file')->store('images', 'public') :
+                $this->updateFile($request, $user) :
                 $user->image_url
         ]);
 
         return response()->json([
             'user' => $user
         ]);
+    }
+
+    public function removeFile()
+    {
+        $user = Auth::user();
+
+        Storage::disk('public')->delete($user->image_url);
+
+        Auth::user()->update([
+            'image_url' => null
+        ]);
+
+        return response()->json([
+            'user' => $user
+        ]);
+    }
+
+    protected function updateFile($request, $user)
+    {
+        Storage::disk('public')->delete($user->image_url);
+
+        return $request->file('file')->store('images', 'public');
     }
 }
