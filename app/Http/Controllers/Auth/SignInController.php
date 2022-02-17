c40e05082516b6e73b9f80de94debb1548a67ce5
Amrilazim3 <a.azim0711@gmail.com>
2022-02-18 00:44:34 +0800

    sign in user with that using social service

--- app/Http/Controllers/Auth/SignInController.php
+++ app/Http/Controllers/Auth/SignInController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -10,6 +11,12 @@ class SignInController extends Controller
 {
     public function signIn(Request $request)
     {
+        $oauthSignIn = $this->oauthSignIn($request);
+
+        if ($oauthSignIn) {
+            return true;
+        } 
+
         $credentials = $request->validate([
             'email' => ['required', 'email', 'exists:users,email'],
             'password' => ['required'],
@@ -23,6 +30,19 @@ class SignInController extends Controller
             return response()->json([
                 'error' => 'something went wrong.'
             ], 422);
+        }
+    }
+
+    protected function oauthSignIn($request)
+    {
+        $oauthservice = User::where('email', $request->email)
+        ->where('password', null)
+        ->first();
+
+        if ($oauthservice) {
+            Auth::login($oauthservice);
+            $request->session()->regenerate();
+            return true;
         }
     }
 
