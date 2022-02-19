e0e69d1ccb09bf6486638ceb8e5a368781a4b22e
Amrilazim3 <a.azim0711@gmail.com>
2022-02-19 10:24:23 +0800

    adding verifyPassword rule

--- app/Http/Controllers/User/ChangePasswordController.php
+++ app/Http/Controllers/User/ChangePasswordController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Rules\ValidateNewPassword;
 use App\Rules\ValidatePassword;
+use App\Rules\VerifyPassword;
 use Illuminate\Support\Facades\Auth;
 
 class ChangePasswordController extends Controller
@@ -22,6 +23,7 @@ class ChangePasswordController extends Controller
             'new_password' => [
                 'required',
                 'min:9',
+                new VerifyPassword,
                 new ValidateNewPassword($user)
             ],
             'new_password_confirmation' => [
--- app/Http/Controllers/User/SetPasswordController.php
+++ app/Http/Controllers/User/SetPasswordController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Rules\VerifyPassword;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -13,7 +14,11 @@ class SetPasswordController extends Controller
         $user = Auth::user();
 
         $request->validate([
-            'password' => 'required|min:9',
+            'password' => [
+                'required',
+                'min:9',
+                new VerifyPassword
+            ],
             'password_confirmation' => 'required|min:9|same:password'
         ]);
 
