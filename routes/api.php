28f5425950289723e647d59612715cf9a2e5d599
Amrilazim3 <a.azim0711@gmail.com>
2022-02-23 07:33:24 +0800

    tweak route method & create route for handling user friends

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use App\Http\Controllers\User\Account\EditProfileController;
 use App\Http\Controllers\User\ChangePasswordController;
 use App\Http\Controllers\User\ForgotPasswordController;
+use App\Http\Controllers\User\FriendController;
 use App\Http\Controllers\User\ResetPasswordController;
 use App\Http\Controllers\User\SetPasswordController;
 
@@ -30,32 +31,31 @@ Route::middleware('auth')->group(function() {
     Route::post('/sign-out', [SignInController::class, 'destroy']);
 
     Route::post('/user/account/profile/edit', [EditProfileController::class, 'edit']);
-    Route::get('/user/account/profile/remove-profile-image', [EditProfileController::class, 'removeFile']);
-    Route::post('/user/account/profile/change-email', [EditProfileController::class, 'changeEmail']);
-    
-    Route::post('/user/account/change-password', [ChangePasswordController::class, 'update']);
-
+    Route::patch('/user/account/profile/remove-profile-image', [EditProfileController::class, 'removeFile']);
+    Route::patch('/user/account/profile/change-email', [EditProfileController::class, 'changeEmail']);
+    Route::patch('/user/account/change-password', [ChangePasswordController::class, 'update']);
     Route::get('/user/account/get-password', [ChangePasswordController::class, 'getPassword']);
-    Route::post('/user/account/set-password', [SetPasswordController::class, 'setPassword']);
+    Route::patch('/user/account/set-password', [SetPasswordController::class, '__invoke']);
+    
+    Route::get('/user/friends', [FriendController::class, 'index']);
+    Route::post('/user/friends/{user}', [FriendController::class, 'follow']);
+    Route::delete('/user/friends/cancel-request/{user}', [FriendController::class, 'cancelRequest']);
+    Route::delete('/user/friends/{user}', [FriendController::class, 'delete']);
 });
 
 // This route can be access by non-authenticated & authenticated users (as long user have password)
 Route::post('/user/account/forgot-password', [ForgotPasswordController::class, 'sendResetLink'])
     ->name('password.email');
-
 // when user click link in the email
 Route::get('/user/account/forgot-password/{token}', [ForgotPasswordController::class, 'handle'])
     ->name('password.reset');
-
 //handle reset password form 
-Route::post('/user/account/reset-password', [ResetPasswordController::class, 'resetPassword']);
-
+Route::patch('/user/account/reset-password', [ResetPasswordController::class, 'resetPassword']);
 
 // Verify email
 Route::get('/email/verify/{id}/{hash}', [VerifyEmailController::class, '__invoke'])
     ->middleware(['signed', 'throttle:6,1'])
     ->name('verification.verify');
-
 // Resend link to verify email
 Route::post('/email/verify/resend', [VerifyEmailController::class, 'resend'])
     ->middleware(['auth:sanctum', 'throttle:6,1'])
