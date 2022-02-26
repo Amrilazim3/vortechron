b3cee2bb700b1a0e23e912556ef0d041c8239acf
Amrilazim3 <a.azim0711@gmail.com>
2022-02-26 21:22:36 +0800

    sendting result of search

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class SearchController extends Controller
+{
+    public function index()
+    {
+        $users = User::search(request(['search']))
+        ->select('id', 'username', 'image_url')
+        ->paginate(5);
+
+        // $posts = Post::search(request(['search']))
+        // ->get();
+
+        return response()->json([
+            'users' => $users
+        ]);
+    }
+}
