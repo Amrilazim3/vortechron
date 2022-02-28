e8bfbe398c205d89eb3b4a10305c459fb3295795
Amrilazim3 <a.azim0711@gmail.com>
2022-02-28 19:56:22 +0800

    make friend api to be less private

--- app/Traits/Friendable.php
+++ app/Traits/Friendable.php
@@ -15,14 +15,14 @@ trait Friendable {
             ->where('requester', $this->id)
             ->get(); // get users that approve current user friend request
         foreach($f1 as $friendship):
-            array_push($friends, User::find($friendship->user_requested));
+            array_push($friends, User::find($friendship->user_requested)->only('id', 'username', 'image_url', 'image_full_url'));
         endforeach;
         $friends2 = array();
         $f2 = Friend::where('status', 1)
             ->where('user_requested', $this->id)
             ->get(); // get users that current user already approve to be friend
         foreach($f2 as $friendship):
-            array_push($friends2, User::find($friendship->requester));
+            array_push($friends2, User::find($friendship->requester)->only('id', 'username', 'image_url', 'image_full_url'));
         endforeach;
         return array_merge($friends, $friends2);
     }
@@ -69,175 +69,18 @@ trait Friendable {
         return collect($this->following())->pluck('id')->toArray();
     }
 
-    // other users is pending request for current user.
-    public function pendingFriendRequests()
+    public function follow($user_requested_id) 
     {
-        $users = array();
-        $friendships = Friend::where('status', 0)
-            ->where('user_requested', $this->id)
-            ->get();
-        foreach($friendships as $friendship):
-            array_push($users, User::find($friendship->requester));
-        endforeach;
-        return $users;
-    }
-
-    // get only 'id' column from result of this.pendingFriendRequests().
-    public function pendingFriendRequestsIds()
-    {
-		return collect($this->pendingFriendRequests())->pluck('id')->toArray();
-    }
-
-    // check if the given '$user_id' exists in pendingFriendRequestsIds()
-	public function hasPendingFriendRequestFrom($user_id)
-    {
-		if(in_array($user_id, $this->pendingFriendRequestsIds())) {
-			return 1;
-		}
-		else {
-			return 0;
-		}
-    }
-
-    // the current user is pendng friend request to other user.
-    public function pendingFriendRequestsSent()
-    {
-		$users = array();
-		$friendships = Friend::where('status', 0)
-            ->where('requester', $this->id)
-            ->get();
-		foreach($friendships as $friendship):
-			array_push($users, User::find($friendship->user_requested));
-		endforeach;
-		return $users;
-	}
-
-    // get only 'id' column from this.pendingFriendRequestsSent()
-	public function pendingFriendRequestsSentIds() 
-    {
-		return collect($this->pendingFriendRequestsSent())->pluck('id')->toArray();
-	}
-
-    // check if current user is making friend request to other users.
-    public function hasPendingFriendRequestSentTo($user_id)
-    {
-		if(in_array($user_id, $this->pendingFriendRequestsSentIds())) {
-			return 1;
-		}
-		else {
-			return 0;
-		}
-    }
-
-    public function cancelFriendRequestSentTo($user_requested_id)
-    {
-        if ($this->hasPendingFriendRequestSentTo($user_requested_id) == 1) {
-            $friendToCancel = Friend::where('user_requested', $user_requested_id)
-                ->where('status', 0);
-
-            $friendToCancel->delete();
-            return 1;
-        } else {
-            return 0;
-        }
-    }
-
-    // check if current user is friend with other user.
-    public function isFriendsWith($id)
-    {
-        if (in_array($id, $this->followingIds())) {
-            return 1;
-        } else {
-            return 0;
-        }
-    }
-
-    // checking for current user before friend with other users.
-    public function addFriend($user_requested_id)
-    {
-        if($this->id === $user_requested_id) {
-			return 0;
-        }
-
-        if($this->isFriendsWith($user_requested_id) === 1) {
-			return "already friends";
-        }
-
-        // current user already sent from request but the requested user not accept yet
-        if($this->hasPendingFriendRequestSentTo($user_requested_id) === 1){ 
-			return "already sent a friend request";
-        }
-
-        // check if other users also has sent friend request automatic add as friend
-        // because both request to be friend with each other.
-        if($this->hasPendingFriendRequestFrom($user_requested_id) === 1){
-			return $this->acceptFriend($user_requested_id);
-		}
-
-        $friendship = Friend::create([
+        $newFollow = Friend::create([
             'requester' => $this->id,
             'user_requested' => $user_requested_id,
-            'status' => 0
+            'status' => 1
         ]);
-        if($friendship) {
-			return 1;
-		}
-		return 0;
-    }
 
-    // making a requester become friend with the current user.
-    public function acceptFriend($requester) 
-    {
-        if($this->hasPendingFriendRequestFrom($requester) === 0) {
-            return 0;
-        }
-        $friendship = Friend::where('requester', $requester)
-            ->where('user_requested', $this->id)
-            ->first();
-        if($friendship) {
-            $friendship->update([
-                'status' => 1
-            ]);
+        if ($newFollow) {
             return 1;
         }
         return 0;
-    }
-
-    // deny the request from other users to be friend with current user.
-    public function denyFriend($requester)
-    {
-        if($this->hasPendingFriendRequestFrom($requester) === 0) {
-            return 0;
-        }
-        $friendship = Friend::where('requester', $requester)
-            ->where('user_requested', $this->id)
-            ->first();
-        if($friendship) {
-            $friendship->delete();
-            return 1;
-        }
-        return 0;
-    }
-
-    public function deleteFriend($user_requested_id)
-    {
-		if($this->id === $user_requested_id) {
-            return 0;
-        }
-		if(in_array($user_requested_id, $this->followersIds()) || in_array($user_requested_id, $this->followingIds())) {
-            $Friendship1 = Friend::where('requester', $user_requested_id)
-            ->where('user_requested', $this->id)
-            ->first();
-            if ($Friendship1) {
-                $Friendship1->delete();
-            }
-            $Friendship2 = Friend::where('user_requested', $user_requested_id)
-            ->where('requester', $this->id)
-            ->first();
-            if ($Friendship2) {
-                $Friendship2->delete();
-            }
-        }
     }
 
     public function unfollow($user_requested_id)
@@ -251,5 +94,15 @@ trait Friendable {
             } else{
                 return 0;
             }
+    }
+
+    // check if current user is friend with other user.
+    public function isFriendsWith($id)
+    {
+        if (in_array($id, $this->followingIds())) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
 }
