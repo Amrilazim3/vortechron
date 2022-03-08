76e6069acb74c75f0773babf90594667e2cf82d8
Amrilazim3 <a.azim0711@gmail.com>
2022-03-08 10:39:52 +0800

    get total followers and following count

--- app/Http/Controllers/User/Account/ProfileController.php
+++ app/Http/Controllers/User/Account/ProfileController.php
@@ -4,19 +4,22 @@ namespace App\Http\Controllers\User\Account;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class ProfileController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $user = Auth::user();
-        $followers = $user->followersIds();
-        $following = $user->followingIds();
+        $user = $request->user();
+        $followersCount = $user::withCount('followers')
+            ->find($user->id)
+            ->followers_count;
+        $followingCount = $user::withCount('following')
+            ->find($user->id)
+            ->following_count;
 
         return response()->json([
-            'followers' => $followers,
-            'following' => $following
+            'followers_count' => $followersCount,
+            'following_count' => $followingCount
         ]);
     }
 }
