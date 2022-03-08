38721574b50b943322fc1173ddbbfab2d89aac1f
Amrilazim3 <a.azim0711@gmail.com>
2022-03-08 22:55:33 +0800

    adding user resource

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'username' => $this->username,
+            'image_url' => $this->image_url,
+            'image_full_url' => $this->image_full_url
+        ];
+    }
+}
