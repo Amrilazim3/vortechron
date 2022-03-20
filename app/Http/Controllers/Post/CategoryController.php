5a7cc7fbea9682972b0a625d9467a48a7ffd2a91
Amrilazim3 <a.azim0711@gmail.com>
2022-03-20 11:57:02 +0800

    show all posts list and show all posts with one category

---
+++ app/Http/Controllers/Post/CategoryController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Post;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\AllPostResource;
+use App\Models\Category;
+use App\Models\Post;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        $categories = Category::all(['name', 'slug']);
+
+        return response()->json([
+            'categories' => $categories
+        ]);
+    }
+
+    public function show(Category $category)
+    {
+        $resPosts = Post::where('category_id', $category->id)->paginate(20);
+        $posts = AllPostResource::collection($resPosts)
+                    ->response()
+                    ->getData();
+
+        return response()->json([
+            'posts' => $posts
+        ]);
+    }
+}
