8d55b9d765321f2fc48f12b33351a192dc71c3e7
Amrilazim3 <a.azim0711@gmail.com>
2022-03-21 23:33:56 +0800

    get a query result of posts

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,22 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use App\Models\User;
-use Illuminate\Http\Request;
 
 class SearchController extends Controller
 {
-    public function searchUsers()
+    public function __invoke()
     {
         $users = User::search(request(['search']))
-        ->select('id', 'username', 'image_url')
-        ->paginate(5);
+            ->select('id', 'username', 'image_url')
+            ->paginate(5);
 
-        // $posts = Post::search(request(['search']))
-        // ->get();
+        $posts = Post::search(request(['search']))
+            ->select('title', 'slug', 'user_id')
+            ->paginate(5);
 
         return response()->json([
-            'users' => $users
+            'users' => $users,
+            'posts' => $posts
         ]);
     }
 }
