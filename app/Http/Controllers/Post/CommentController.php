8b48a1c57fd08543dc7f6c0150f7fe6c2403db83
Amrilazim3 <a.azim0711@gmail.com>
2022-03-23 18:47:20 +0800

    creating methods to show all comment and create comment

---
+++ app/Http/Controllers/Post/CommentController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Post;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\UserCommentResource;
+use App\Models\Comment;
+use App\Models\Post;
+use App\Traits\ArrayPaginable;
+use Illuminate\Http\Request;
+
+class CommentController extends Controller
+{
+    use ArrayPaginable;
+
+    public function index(Post $post)
+    {
+        $resComments = $this->paginate(
+            $post->comments->sortByDesc('created_at'),
+            20
+        );
+
+        $comments = UserCommentResource::collection($resComments)
+            ->response()
+            ->getData();
+
+        return response()->json([
+            'comments' => $comments
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'comment' => 'required|max:500'
+        ]);
+
+        Comment::create([
+            'post_id' => $request->postId,
+            'user_id' => $request->userId,
+            'body' => $request->comment
+        ]);
+
+        return true;
+    }
+}
